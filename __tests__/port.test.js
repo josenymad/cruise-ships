e4b275f5be42ba45d181204b9f49c455d1e56cc3
Jose Wills <jose.wills3@gmail.com>
2023-03-23 21:01:14 +0000

    tests dried

--- __tests__/port.test.js
+++ __tests__/port.test.js
@@ -2,66 +2,61 @@ const Ship = require('../src/ship');
 const Port = require('../src/port');
 const Itinerary = require('../src/itinerary');
 
-describe('port constructor', () => {
-    it('port is an object', () => {
-        const port = new Port();
-        expect(port).toBeInstanceOf(Object);
+describe('Ports', () => {
+    let port1;
+    let port2;
+    let port3;
+    let itinerary;
+    let ship;
+
+    beforeEach(() => {
+        port1 = new Port('Port 1');
+        port2 = new Port('Port 2');
+        port3 = new Port('Port 3');
+        itinerary = new Itinerary([port1, port2, port3]);
+        ship = new Ship(itinerary);
     });
 
-    it('port has a name', () => {
-        const port1 = new Port('Port 1');
-        expect(port1.name).toBe('Port 1');
+    describe('port constructor', () => {
+        it('port is an object', () => {
+            expect(port1).toBeInstanceOf(Object);
+        });
+
+        it('port has a name', () => {
+            expect(port1.name).toBe('Port 1');
+        });
+
+        it('has a ships property', () => {
+            expect(port1).toHaveProperty('ships');
+        });
     });
 
-    it('has a ships property', () => {
-        const port1 = new Port('Port 1');
-        expect(port1).toHaveProperty('ships');
-    });
-});
+    describe('method to add ships which have docked', () => {
+        it('checks the ship to add has docked at port', () => {
+            ship.setSail();
+            ship.dock();
+            expect(port1.addShip(ship)).toEqual('This ship is not currently docked at this port');
+        });
 
-describe('method to add ships which have docked', () => {
-    it('checks the ship to add has docked at port', () => {
-        const port1 = new Port('Port 1');
-        const port2 = new Port('Port 2');
-        const itinerary = new Itinerary([port1, port2]);
-        const ship = new Ship(itinerary);
-        ship.setSail();
-        ship.dock();
-        expect(port1.addShip(ship)).toEqual('This ship is not currently docked at this port');
+        it('adds docked ship to ships property array', () => {
+            ship.setSail();
+            ship.dock();
+            expect(port2.ships).toContain(ship);
+        });
     });
 
-    it('adds docked ship to ships property array', () => {
-        const port1 = new Port('Port 1');
-        const port2 = new Port('Port 2');
-        const itinerary = new Itinerary([port1, port2]);
-        const ship = new Ship(itinerary);
-        ship.setSail();
-        ship.dock();
-        expect(port2.ships).toContain(ship);
-    });
-});
+    describe('method to remove ship from ships array', () => {
+        it('checks ship has set sail before removing from ships array', () => {
+            ship.setSail();
+            ship.dock();
+            expect(port2.removeShip(ship)).toEqual('This ship is still docked at this port');
+        });
 
-describe('method to remove ship from ships array', () => {
-    it('checks ship has set sail before removing from ships array', () => {
-        const port1 = new Port('Port 1');
-        const port2 = new Port('Port 2');
-        const port3 = new Port('Port 3');
-        const itinerary = new Itinerary([port1, port2, port3]);
-        const ship = new Ship(itinerary);
-        ship.setSail();
-        ship.dock();
-        expect(port2.removeShip(ship)).toEqual('This ship is still docked at this port');
-    });
-
-    it('removes a ship which set sail from ships property array', () => {
-        const port1 = new Port('Port 1');
-        const port2 = new Port('Port 2');
-        const port3 = new Port('Port 3');
-        const itinerary = new Itinerary([port1, port2, port3]);
-        const ship = new Ship(itinerary);
-        ship.setSail();
-        ship.dock();
-        ship.setSail();
-        expect(port2.ships).not.toContain(ship);
+        it('removes a ship which set sail from ships property array', () => {
+            ship.setSail();
+            ship.dock();
+            ship.setSail();
+            expect(port2.ships).not.toContain(ship);
+        });
     });
 });
--- __tests__/ship.test.js
+++ __tests__/ship.test.js
@@ -2,137 +2,98 @@ const Ship = require('../src/ship');
 const Port = require('../src/port');
 const Itinerary = require('../src/itinerary');
 
-describe('cruise ship constructor', () => {
-    it('is an object', () => {
-        const port1 = new Port('Port 1');
-        const port2 = new Port('Port 2');
-        const itinerary = new Itinerary([port1, port2]);
-        const ship = new Ship(itinerary);
-        expect(ship).toBeInstanceOf(Object);
+describe('Ships', () => {
+    let port1;
+    let port2;
+    let port3;
+    let itinerary;
+    let ship;
+
+    beforeEach(() => {
+        port1 = new Port('Port 1');
+        port2 = new Port('Port 2');
+        port3 = new Port('Port 3');
+        itinerary = new Itinerary([port1, port2, port3]);
+        ship = new Ship(itinerary);
     });
 
-    it('has a current port property', () => {
-        const port1 = new Port('Port 1');
-        const port2 = new Port('Port 2');
-        const itinerary = new Itinerary([port1, port2]);
-        const ship = new Ship(itinerary);
-        expect(ship).toHaveProperty('currentPort');
+    describe('cruise ship constructor', () => {
+        it('is an object', () => {
+            expect(ship).toBeInstanceOf(Object);
+        });
+
+        it('has a current port property', () => {
+            expect(ship).toHaveProperty('currentPort');
+        });
+
+        it('has a previous port property', () => {
+            expect(ship).toHaveProperty('previousPort');
+        });
+
+        it('has an itinerary', () => {
+            expect(ship.itinerary).toBe(itinerary);
+        });
+
+        it('has a starting port', () => {
+            expect(ship.currentPort).toBe(port1);
+        });
+
+        it('has an itinerary count property', () => {
+            expect(ship).toHaveProperty('itineraryCount');
+        });
+
+        it('keeps track of itinerary progress', () => {
+            ship.setSail();
+            expect(ship.itineraryCount).toBe(1);
+
+            ship.dock();
+            ship.setSail();
+            expect(ship.itineraryCount).toBe(2);
+        });
+
+        it('gets added to port on instantiation', () => {
+            expect(port1.ships).toContain(ship);
+        });
     });
 
-    it('has a previous port property', () => {
-        const port1 = new Port('Port 1');
-        const port2 = new Port('Port 2');
-        const itinerary = new Itinerary([port1, port2]);
-        const ship = new Ship(itinerary);
-        expect(ship).toHaveProperty('previousPort');
+    describe('set sail', () => {
+        it('can set sail', () => {
+            ship.setSail();
+            expect(ship.currentPort).toBeFalsy();
+        });
+
+        it('sets previous port property to the current port', () => {
+            ship.setSail();
+            expect(ship.previousPort).toBe(port1);
+        });
+
+        it('cannot sail further than its itinerary', () => {
+            ship.setSail();
+            ship.dock();
+            ship.setSail();
+            ship.dock();
+            expect(() => ship.setSail()).toThrowError('End of itinerary reached');
+        });
+
+        it('removes ship from previous port\'s ships array', () => {
+            ship.setSail();
+            expect(port1.ships).not.toContain(ship);
+        });
     });
 
-    it('has an itinerary', () => {
-        const port1 = new Port('Port 1');
-        const port2 = new Port('Port 2');
-        const itinerary = new Itinerary([port1, port2]);
-        const ship = new Ship(itinerary);
-        expect(ship.itinerary).toBe(itinerary);
-    });
+    describe('docking', () => {
+        it('docks at next port in itinerary', () => {
+            ship.setSail();
+            ship.dock();
+            ship.setSail();
+            ship.dock();
+            expect(ship.currentPort).toBe(port3);
+        });
 
-    it('has a starting port', () => {
-        const port1 = new Port('Port 1');
-        const port2 = new Port('Port 2');
-        const itinerary = new Itinerary([port1, port2]);
-        const ship = new Ship(itinerary);
-        expect(ship.currentPort).toBe(port1);
-    });
-
-    it('has an itinerary count property', () => {
-        const port1 = new Port('Port 1');
-        const port2 = new Port('Port 2');
-        const itinerary = new Itinerary([port1, port2]);
-        const ship = new Ship(itinerary);
-        expect(ship).toHaveProperty('itineraryCount');
-    });
-
-    it('keeps track of itinerary progress', () => {
-        const port1 = new Port('Port 1');
-        const port2 = new Port('Port 2');
-        const port3 = new Port('Port 3');
-        const itinerary = new Itinerary([port1, port2, port3]);
-        const ship = new Ship(itinerary);
-        ship.setSail();
-        expect(ship.itineraryCount).toBe(1);
-
-        ship.dock();
-        ship.setSail();
-        expect(ship.itineraryCount).toBe(2);
-    });
-
-    it('gets added to port on instantiation', () => {
-        const port1 = new Port('Port 1');
-        const itinerary = new Itinerary([port1]);
-        const ship = new Ship(itinerary);
-        expect(port1.ships).toContain(ship);
-    });
-});
-
-describe('set sail', () => {
-    it('can set sail', () => {
-        const port1 = new Port('Port 1');
-        const port2 = new Port('Port 2');
-        const itinerary = new Itinerary([port1, port2]);
-        const ship = new Ship(itinerary);
-        ship.setSail();
-        expect(ship.currentPort).toBeFalsy();
-    });
-
-    it('sets previous port property to the current port', () => {
-        const port1 = new Port('Port 1');
-        const port2 = new Port('Port 2');
-        const itinerary = new Itinerary([port1, port2]);
-        const ship = new Ship(itinerary);
-        ship.setSail();
-        expect(ship.previousPort).toBe(port1);
-    });
-
-    it('cannot sail further than its itinerary', () => {
-        const port1 = new Port('Port 1');
-        const port2 = new Port('Port 2');
-        const itinerary = new Itinerary([port1, port2]);
-        const ship = new Ship(itinerary);
-        ship.setSail();
-        ship.dock();
-        expect(() => ship.setSail()).toThrowError('End of itinerary reached');
-      });
-
-      it('removes ship from previous port\'s ships array', () => {
-        const port1 = new Port('Port 1');
-        const port2 = new Port('Port 2');
-        const itinerary = new Itinerary([port1, port2]);
-        const ship = new Ship(itinerary);
-        ship.setSail();
-        expect(port1.ships).not.toContain(ship);
-      });
-});
-
-describe('docking', () => {
-    it('docks at next port in itinerary', () => {
-        const port1 = new Port('Port 1');
-        const port2 = new Port('Port 2');
-        const port3 = new Port('Port 3');
-        const itinerary = new Itinerary([port1, port2, port3]);
-        const ship = new Ship(itinerary);
-        ship.setSail();
-        ship.dock();
-        ship.setSail();
-        ship.dock();
-        expect(ship.currentPort).toBe(port3);
-    });
-
-    it('adds ship to port\'s ships array', () => {
-        const port1 = new Port('Port 1');
-        const port2 = new Port('Port 2');
-        const itinerary = new Itinerary([port1, port2]);
-        const ship = new Ship(itinerary);
-        ship.setSail();
-        ship.dock();
-        expect(port2.ships).toContain(ship);
+        it('adds ship to port\'s ships array', () => {
+            ship.setSail();
+            ship.dock();
+            expect(port2.ships).toContain(ship);
+        });
     });
 });
